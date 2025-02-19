32b74b34ca89875df6256a820904381bc17fc1f0
Muzamil Chouhan <muzamil.chouhaan@gmail.com>
2025-02-20 00:17:54 +0500

    my new Updates

---
+++ build/precache-manifest.391eb216d61bbb93a577ff531ee8e0c5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac595fac268a27e9ac33d1a392caa6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "793c7ba038556f9e9d4a",
+    "url": "/static/css/2.194f605c.chunk.css"
+  },
+  {
+    "revision": "793c7ba038556f9e9d4a",
+    "url": "/static/js/2.c106cea4.chunk.js"
+  },
+  {
+    "revision": "ec6378bf61c787815d098c330adf52c5",
+    "url": "/static/js/2.c106cea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58d5b61456d08c474e80",
+    "url": "/static/js/main.2bcdd325.chunk.js"
+  },
+  {
+    "revision": "7c8285640ffae6d0374b",
+    "url": "/static/js/runtime-main.f7586608.js"
+  },
+  {
+    "revision": "5cf0d2a65bf5c2ac7ae56d268866d98d",
+    "url": "/static/media/img.5cf0d2a6.jpeg"
+  },
+  {
+    "revision": "834c3fd05aa42855ea27cf0f2f9a415c",
+    "url": "/static/media/img2.834c3fd0.jpeg"
+  }
+]);
